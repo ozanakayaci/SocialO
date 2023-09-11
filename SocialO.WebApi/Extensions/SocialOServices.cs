bbc00f7c18528aab4955d02832b078ed1657d0e6
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-09-11 20:38:04 +0300

    services added

---
+++ SocialO.WebApi/Extensions/SocialOServices.cs
@@ -0,0 +1,34 @@
+using SocialO.BL.Abstract;
+using SocialO.BL.Concrete;
+using SocialO.DAL.Repository.Abstract;
+using SocialO.DAL.Repository.Concrete;
+
+namespace SocialO.WebApi.Extensions
+{
+	public static class SocialOServices
+	{
+		public static IServiceCollection AddSocialOServices(this IServiceCollection services)
+		{
+
+			services.AddScoped<IFollowerRelationshipRepository, FollowerRelationshipRepository>();
+			services.AddScoped<IFollowerRelationshipManager, FollowerRelationshipManager>();
+
+			services.AddScoped<IPostRepository, PostRepository>();
+			services.AddScoped<IPostManager, PostManager>();
+
+			services.AddScoped<IPostCommentRepository, PostCommentRepository>();
+			services.AddScoped<IPostCommentManager, PostCommentManager>();
+
+			services.AddScoped<IPostFavoriteRepository, PostFavoriteRepository>();
+			services.AddScoped<IPostFavoriteManager, PostFavoriteManager>();
+
+			services.AddScoped<IUserRepository, UserRepository>();
+			services.AddScoped<IUserManager, UserManager>();
+
+			services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+			services.AddScoped<IUserProfileManager, UserProfileManager>();
+
+			return services;
+		}
+	}
+}
--- SocialO.WebApi/Program.cs
+++ SocialO.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using SocialO.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +9,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+builder.Services.AddSocialOServices();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
