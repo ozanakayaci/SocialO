bfbed78c0abfec2a91f746517cc4061e7be5322b
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-09-30 02:41:00 +0300

    navbar

--- SocialO.WebUI/ClientApp/src/components/Navbar/Navbar.js
+++ SocialO.WebUI/ClientApp/src/components/Navbar/Navbar.js
@@ -1,38 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css"; // CSS dosyasını ekledik
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/socialo/socialoSlice";
+
+//css
+import "./Navbar.css";
+
+//component
+import Search from "./Search/Search";
+
+//react router
+import { Link, redirect } from "react-router-dom";
 
 function Navbar() {
   const dispatch = useDispatch();
+
+  const isAuthenticated = useSelector((state) => state.socialo.isAuthenticated);
 
   const handleLogout = () => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("user");
     dispatch(logout());
 
-    window.location.reload();
+    redirect("/");
   };
 
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <h1>
-          <Link className="nav-link" to="/">
-            SocialO
-          </Link>
-        </h1>
-      </div>
-      <div className="nav-links">
-        <Link className="nav-link" to="/home">
-          Home
+    <div className="navbar">
+      <div className="navbar-comp">
+        <Link className="navbar-button logo" to="/">
+          Social<span>-</span>O
         </Link>
-
-        <button onClick={handleLogout}>Logout</button>
+        {isAuthenticated && (
+          <div className="category-buttons">
+            <Link className="navbar-button" to="/home">
+              Home
+            </Link>
+            <Link className="navbar-button" to="/profile">
+              Profile
+            </Link>
+            <Link className="navbar-button" onClick={handleLogout}>
+              Logout
+            </Link>
+          </div>
+        )}
+        <Search />
       </div>
-    </nav>
+    </div>
   );
 }
 
