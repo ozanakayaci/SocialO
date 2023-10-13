c65c1ec17c104f0f3595a1d12fdc646953891a22
Ozan Ali Kayacı <oakayaci@hotmail.com>
2023-10-13 04:26:33 +0300

    routes authenticate

--- SocialO.React/src/App.jsx
+++ SocialO.React/src/App.jsx
@@ -9,20 +9,33 @@ import ErrorPage from "./pages/ErrorPage";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
 import Settings from "./pages/Settings";
+import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.socialo.isAuthenticated);
+
   return (
     <>
+      {isAuthenticated && <Navbar />}
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/settings" element={<Settings />} />
-        
-        <Route path="*" element={<ErrorPage />} />
+        {!isAuthenticated ? (
+          <>
+            <Route path="/" element={<Register />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<ErrorPage />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<ErrorPage />} />
+          </>
+        )}
       </Routes>
     </>
   );
