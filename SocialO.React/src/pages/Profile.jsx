92cce3449d78c9e84aae911de1eabd831acb7ca2
Ozan Ali Kayacı <oakayaci@hotmail.com>
2023-10-13 23:58:51 +0300

    profile page design

--- SocialO.React/src/pages/Profile.jsx
+++ SocialO.React/src/pages/Profile.jsx
@@ -1,9 +1,127 @@
-import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+import Flow from "../components/Flow";
+import { Avatar, Button } from "@mui/material";
+import { useSelector } from "react-redux";
 
 function Profile() {
+  const username = useParams().username;
+
+  const stateUsername = useSelector((state) => state.socialo.userName);
+
+  const [profileData, setProfileData] = useState({
+    about: "undefined",
+    dateOfBirth: "undefined",
+    dateRegistered: "undefined",
+    favoriteCount: "undefined",
+    firstName: "undefined",
+    followerCount: "undefined",
+    followingCount: "undefined",
+    gender: "undefined",
+    id: "undefined",
+    lastName: "undefined",
+    postCount: "undefined",
+    username: "undefined",
+  });
+  const [isFollowed, setIsFollowed] = useState(true);
+
+  const [OwnPost] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5211/api/Users/${username}`)
+      .then((response) => {
+        setProfileData(response.data);
+      });
+    console.log(profileData);
+  }, []);
+
+  function FollowHandler() {
+    setIsFollowed(!isFollowed);
+  }
   return (
     <>
-      <Navbar />
+      <div className="sm:max-w-screen-sm w-full px-5 pb-4 sm:px-24 mt-12 bg-gradient-to-b from-blue-100 rounded-lg	 ">
+        <div className="flex justify-center w-full text-3xl my-10 relative">
+          <div className="absolute bottom-1">
+            <Avatar sx={{ width: 80, height: 80 }}>
+              {profileData.username[0].toUpperCase()}
+            </Avatar>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className="flex flex-row items-center w-full justify-between ">
+              <div className="flex flex-row items-center ">
+                <Link
+                  to={`/${profileData.username}`}
+                  className="hover:underline text-2xl"
+                >
+                  <h2 className="font-semibold text-gray-900 -mt-1 text-ellipsis line-clamp-1">
+                    {profileData.username}
+                  </h2>
+                </Link>
+                <div className="ml-1 text-ellipsis line-clamp-1">
+                  @{profileData.username}
+                </div>
+              </div>
+              {profileData.username !== stateUsername &&
+                (isFollowed ? (
+                  <div>
+                    <button
+                      onClick={FollowHandler}
+                      type="button"
+                      className="inline-block rounded-3xl bg-black w-28 px-6 pb-2 pt-2.5 text-xs uppercase leading-normal text-white font-bold"
+                    >
+                      Unfollow
+                    </button>
+                  </div>
+                ) : (
+                  <div>
+                    <button
+                      onClick={FollowHandler}
+                      type="button"
+                      className="inline-block  rounded-3xl bg-blue-400 w-28 px-6 pb-2 pt-2.5 text-xs uppercase leading-normal text-black font-bold"
+                    >
+                      Follow
+                    </button>
+                  </div>
+                ))}
+            </div>
+            <div className="flex flex-row">
+              <div className="text-slate-400 font-light">
+                <span className="text-black font-bold">
+                  {profileData.postCount}{" "}
+                </span>
+                Posts
+              </div>
+              <div className="ml-3 text-slate-400 font-light">
+                <span className="text-black font-bold">
+                  {profileData.followerCount}{" "}
+                </span>
+                Follower
+              </div>
+              <div className="ml-3 text-slate-400 font-light">
+                <span className="text-black font-bold">
+                  {profileData.followingCount}{" "}
+                </span>
+                Following
+              </div>
+            </div>
+            <div>
+              <div className="whitespace-pre-wrap	">
+                {profileData.about ? profileData.about : " tettetteeett"}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <>
+        <Flow OwnPost={OwnPost} profileId={profileData.id} />
+      </>
     </>
   );
 }
