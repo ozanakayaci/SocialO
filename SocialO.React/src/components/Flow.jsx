52411cc188be5f59f30cb6573ddf2046b2751180
Ozan Ali Kayacı <oakayaci@hotmail.com>
2023-10-13 21:23:49 +0300

    visual and api bug fixes

--- SocialO.React/src/components/Flow.jsx
+++ SocialO.React/src/components/Flow.jsx
@@ -10,7 +10,6 @@ import { logout, login } from "../redux/socialo/socialoSlice";
 import PostCard from "./PostCard";
 
 import PropTypes from "prop-types";
-import { Card } from "@mui/material";
 
 function Flow({ OwnPost, profileId }) {
   const dispatch = useDispatch();
@@ -53,13 +52,21 @@ function Flow({ OwnPost, profileId }) {
                 )}`
               )
               .then((response) => {
-                console.log("response", response.data);
-                dispatch(login(response.data));
+                console.log("response", response);
+                if (response.data === "") {
+                  setTest(4);
+                } else {
+                  dispatch(login(response.data));
+                }
                 setTest(2);
               })
-              .catch(() => {
-                dispatch(logout());
-                navigate("/");
+              .catch((error) => {
+                if (error.response.status === 400) {
+                  setTest(5);
+                } else {
+                  dispatch(logout());
+                  navigate("/");
+                }
               });
           } else {
             dispatch(logout());
@@ -71,13 +78,13 @@ function Flow({ OwnPost, profileId }) {
   }, [test, profileId]);
 
   return (
-    <Card className="flex flex-col mt-14 items-center sm:min-w-full  ">
+    <div className="flex flex-col mt-14 items-center sm:min-w-full  ">
       {posts.length < 1 && <div className="text-red-500">{message}</div>}
 
       {posts.map((post) => (
         <PostCard key={post.postId} post={post} />
       ))}
-    </Card>
+    </div>
   );
 }
 
