8ea7fae0e69e7c3ecffed713f14d61be7734a9e8
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-10-15 01:24:38 +0300

    UserProfileController created for edit profile

---
+++ SocialO.WebApi/Controllers/UserProfileController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialO.BL.Abstract;
+using SocialO.BL.Concrete;
+using SocialO.DAL.DBContexts;
+using SocialO.Entities.Concrete;
+using SocialO.WebApi.Models.UserModels.Register;
+using SocialO.WebApi.Models.UserProfile;
+
+namespace SocialO.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+
+        private readonly SqlDBContext _context;
+        private readonly UserProfileManager _userProfileManager;
+
+        public UserProfileController(SqlDBContext context)
+        {
+            _context = context;
+            _userProfileManager = new UserProfileManager();
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> PutProfile([FromBody] EditUserProfileModel userProfileModel)
+        {
+
+            var existUserProfile = _userProfileManager.GetBy(up => up.UserId == userProfileModel.UserId).Result;
+
+            if (existUserProfile == null)
+            {
+                return BadRequest();
+
+            }
+
+            existUserProfile.FirstName = userProfileModel.FirstName;
+            existUserProfile.LastName  = userProfileModel.LastName;
+            existUserProfile.Gender = userProfileModel.Gender;
+            existUserProfile.DateOfBirth = userProfileModel.DateOfBirth;
+            existUserProfile.About = userProfileModel.About;
+            existUserProfile.DateUpdated = DateTime.Now;
+
+
+            
+
+
+            int result = _userProfileManager.UpdateAsync(existUserProfile).Result;
+
+            return Ok(result);
+        }
+
+
+
+    }
+}
---
+++ SocialO.WebApi/Models/UserProfile/EditUserProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using SocialO.Entities.Concrete;
+
+namespace SocialO.WebApi.Models.UserProfile
+{
+    public class EditUserProfileModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public char? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? About { get; set; }
+        public int UserId { get; set; }
+        
+
+    }
+}
