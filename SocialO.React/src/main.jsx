0da4e449abb63d0597abc4859a559e134c65dba4
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-10-15 03:20:40 +0300

    mui datepicker

--- SocialO.React/src/main.jsx
+++ SocialO.React/src/main.jsx
@@ -9,28 +9,33 @@ import { BrowserRouter } from "react-router-dom";
 //react-toastify
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+//MUI
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ToastContainer
-      position="bottom-center"
-      autoClose={3000}
-      limit={2}
-      hideProgressBar={false}
-      newestOnTop
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        limit={2}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
