2e0930ed50f6b9d0051735ac4d57e0b858946da3
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-11-11 02:35:11 +0300

    Search fix If the user is not found, it now says 'Type Something Else' else.

--- SocialO.React/src/pages/Search.jsx
+++ SocialO.React/src/pages/Search.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 function Search() {
   const [searchText, setSearchText] = useState("");
 
+  const [notFound, setNotFound] = useState(false);
+
   const [usersData, setUsersData] = useState([
     {
       id: -1,
@@ -32,6 +34,22 @@ function Search() {
             )
             .then((response) => {
               setUsersData(response.data);
+              setNotFound(false);
+            })
+            .catch((error) => {
+              if (error.response.status == 404) {
+                setNotFound(true);
+                setUsersData([
+                  {
+                    id: -1,
+                    username: "undefined",
+                    about: "undefined",
+                    name: "undefined",
+                    followerCount: -1,
+                    followingCount: -1,
+                  },
+                ]);
+              }
             });
         }
       },
@@ -49,18 +67,20 @@ function Search() {
           id="outlined-basic"
           label="Search"
           variant="outlined"
-          placeholder="Username "
+          placeholder="Search by username"
           autoFocus
           value={searchText}
           onChange={(e) => {
             setSearchText(e.target.value);
           }}
-        />
+        />  
       </div>
 
       {searchText != "" && usersData[0].id != -1 ? (
+        // if user found
         usersData.map((user, i) => <ProfileCard key={i} user={user} />)
-      ) : (
+      ) : // if no user found
+      notFound ? (
         <div className="mt-20 text-2xl md:text-5xl font-semibold text-blue-500 flex flex-row items-center h-36">
           <div className=" h-12 w-12 flex items-center justify-center rounded-lg fill-blue-500  text-blue-500 mt-1 md:mt-2 ">
             <svg
@@ -71,7 +91,21 @@ function Search() {
               <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
             </svg>
           </div>
-          Start Searching...
+          Type Something Else...
+        </div>
+      ) : (
+        // if no search text
+        <div className="mt-20 text-2xl md:text-5xl font-semibold text-blue-500 flex flex-row items-center h-36">
+          <div className=" h-12 w-12 flex items-center justify-center rounded-lg fill-blue-500  text-blue-500 mt-1 md:mt-2 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 md:h-10 w-6 md:w-10"
+              viewBox="0 0 24 24"
+            >
+              <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
+            </svg>
+          </div>
+          Start Typing...
         </div>
       )}
     </>
